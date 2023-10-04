4b4045a4c6b9b91b9a714aae4fb70514b1da3e49
prstk2 <146347114+prstk2@users.noreply.github.com>
2023-10-04 17:00:04 +0300

    Update pong.py

--- pong.py
+++ pong.py
@@ -28,28 +28,79 @@ class Player(GameSprite):
             p2.rect.y += p2.speed
 
 
+class Ball(GameSprite):
+    def update(self):
+        global speedx
+        global speedy
+        global score_1
+        global score_2
+
+        self.rect.x += speedx
+        self.rect.y += speedy
+
+        if self.rect.y <= 0 or self.rect.y >= 450:
+            speedy *= -1
+
+        if self.rect.x <= -50:
+            score_2 += 1
+            self.rect.x = 250
+            self.rect.y = 350
+        if self.rect.x >= 700:
+            score_1 += 1
+            self.rect.x = 250
+            self.rect.y = 350
+        
+        if self.rect.colliderect(p1.rect):
+            speedx = 7 
+            speedy *= -1
+        if self.rect.colliderect(p2.rect):
+            speedx = -7
+            speedy *= -1
+
+
+
 WINDOW_SIZE = (700, 500)
 FPS = 60
 window = display.set_mode(WINDOW_SIZE)
-black = (10, 40, 50)
+background = (0, 153, 153)
+font.init()
+font = font.SysFont("Arial", 50)
 
-p1 = Player(10, 200, 5, "pong_p1.png", 50, 100)
-p2 = Player(640, 200, 5, "pong_p1.png", 50, 100)
-
+p1 = Player(10, 200, 5, "platform.png", 50, 100)
+score_1 = 0
+p2 = Player(640, 200, 5, "platform.png", 50, 100)
+score_2 = 0
+ball = Ball(250, 300, 0, "ball.png", 50, 50)
+speedx = 7
+speedy = 5
 clock = time.Clock()
 running = True
+finished = False
+
 
 while running:
-    window.fill(black)
     for e in event.get():
         if e.type == QUIT:
             running = False  
-    
-    p1.reset()
-    p2.reset()
 
-    p1.update()
-    p1.update()
+    if finished != True:
+        window.fill(background)
+        p1.reset()
+        p2.reset()
+        ball.reset()
+
+        p1.update()
+        p1.update()
+        ball.update()
+
+        if score_1 >= 3:
+            finished = True
+            text1 = font.render("Player 1 wins", False, (50, 50, 50))
+            window.blit(text1, (250, 200))
+        if score_2 >= 3:
+            finished = True
+            text1 = font.render("Player 2 wins", False, (50, 50, 50))
+            window.blit(text1, (250, 200))
 
     display.update()
     clock.tick(FPS)
